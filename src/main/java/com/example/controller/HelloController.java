0b4fc83495f3560f4a36a94ecf9a63475d177d4e
wangtenfee <wangtenfee@163.com>
2017-01-12 10:24:14 +0800

    完成第二章

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -1,14 +1,28 @@
 package com.example.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.twitter.api.SearchResults;
+import org.springframework.social.twitter.api.Tweet;
+import org.springframework.social.twitter.api.Twitter;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+@Controller /* @Controller只能修饰类,表示该类是一个控制器类,用一解析用户请求的URI */
 public class HelloController {
+	@Autowired
+	private Twitter twitter;
+
 	@RequestMapping("/")
-	@ResponseBody
-	public String hello() {
-		return "Hello, world!";
+	public String hello(@RequestParam(defaultValue = "masterSpringMVC4") String search, Model model) {
+		SearchResults searchResults = twitter.searchOperations().search(search);
+		List<String> tweets = searchResults.getTweets().stream().map(Tweet::getText).collect(Collectors.toList());
+		model.addAttribute("tweets", tweets);
+		return "resultPage";
 	}
 }
