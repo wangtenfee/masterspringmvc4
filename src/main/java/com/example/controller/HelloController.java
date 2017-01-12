8a1c183b8b48527b186b709d1c623fd8f26667fe
wangtenfee <wangtenfee@163.com>
2017-01-13 00:53:23 +0800

    modify the defaultvalue of search

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -19,7 +19,7 @@ public class HelloController {
 	private Twitter twitter;
 
 	@RequestMapping("/")
-	public String hello(@RequestParam(defaultValue = "masterSpringMVC4") String search, Model model) {
+	public String hello(@RequestParam(defaultValue = "subway") String search, Model model) {
 		SearchResults searchResults = twitter.searchOperations().search(search);
 		List<String> tweets = searchResults.getTweets().stream().map(Tweet::getText).collect(Collectors.toList());
 		model.addAttribute("tweets", tweets);
