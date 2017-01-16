27fededd4f309d153664bbbf4a8647008a269e6f
wangtenfee <wangtenfee@163.com>
2017-01-16 15:32:11 +0800

    添加校验

---
+++ src/main/java/com/example/config/WebConfiguration.java
@@ -0,0 +1,17 @@
+package com.example.config;
+
+import java.time.LocalDate;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.example.date.USLocalDateFormatter;
+
+@Configuration
+public class WebConfiguration extends WebMvcConfigurerAdapter {
+	@Override
+	public void addFormatters(FormatterRegistry registry) {
+		registry.addFormatterForFieldType(LocalDate.class, new USLocalDateFormatter());
+	}
+}
--- src/main/java/com/example/controller/ProfileController.java
+++ src/main/java/com/example/controller/ProfileController.java
@@ -1,9 +1,16 @@
 package com.example.controller;
 
+import java.util.Locale;
+
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.date.USLocalDateFormatter;
 import com.example.doman.ProfileForm;
 
 @Controller
@@ -13,9 +20,23 @@ public class ProfileController {
 		return "profile/profilePage";
 	}
 
+//	@RequestMapping(value = "/profile", method = RequestMethod.POST)
+//	public String saveProfile(ProfileForm profileForm) {
+//		System.out.println("save ok" + profileForm);
+//		return "redirect:/profile";
+//	}
+	
 	@RequestMapping(value = "/profile", method = RequestMethod.POST)
-	public String saveProfile(ProfileForm profileForm) {
+	public String saveProfile(@Valid ProfileForm profileForm, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return "profile/profilePage";
+		}
 		System.out.println("save ok" + profileForm);
 		return "redirect:/profile";
+	}
+	
+	@ModelAttribute("dateFormat")
+	public String localeFormat(Locale locale) {
+		return USLocalDateFormatter.getPattern(locale);
 	}
 }
