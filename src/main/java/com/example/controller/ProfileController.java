5fdb419224fae2c09e440f847dc85bf7e1405465
wangtenfee <wangtenfee@163.com>
2017-01-16 16:36:59 +0800

    给displayprofile增加@ModelAttribute("profileForm")

--- src/main/java/com/example/controller/ProfileController.java
+++ src/main/java/com/example/controller/ProfileController.java
@@ -5,6 +5,7 @@ import java.util.Locale;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,8 @@ import com.example.doman.ProfileForm;
 
 @Controller
 public class ProfileController {
+	
+	@ModelAttribute("profileForm")
 	@RequestMapping("/profile")
 	public String displayProfile() {
 		return "profile/profilePage";
