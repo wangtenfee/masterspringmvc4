d981fad502ab58b0df92b67fe0af62a64614ca49
wangtenfee <wangtenfee@163.com>
2017-01-16 12:00:47 +0800

    对用户的输入进行过滤

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -3,6 +3,8 @@ package com.example.controller;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.twitter.api.SearchResults;
 import org.springframework.social.twitter.api.Tweet;
@@ -10,27 +12,31 @@ import org.springframework.social.twitter.api.Twitter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller /* @Controller只能修饰类,表示该类是一个控制器类,用一解析用户请求的URI */
 public class HelloController {
 	@Autowired
 	private Twitter twitter;
 
-//	@RequestMapping("/")
-//	public String hello(@RequestParam(defaultValue = "subway") String search, Model model) {
-//		SearchResults searchResults = twitter.searchOperations().search(search);
-//		List<String> tweets = searchResults.getTweets().stream().map(Tweet::getText).collect(Collectors.toList());
-//		model.addAttribute("tweets", tweets);
-//		return "resultPage";
-//	}
-	
+	// @RequestMapping("/")
+	// public String hello(@RequestParam(defaultValue = "subway") String search,
+	// Model model) {
+	// SearchResults searchResults = twitter.searchOperations().search(search);
+	// List<String> tweets =
+	// searchResults.getTweets().stream().map(Tweet::getText).collect(Collectors.toList());
+	// model.addAttribute("tweets", tweets);
+	// return "resultPage";
+	// }
+
 	@RequestMapping("/")
 	public String home() {
 		return "searchPage";
 	}
-	
+
 	@RequestMapping("/result")
 	public String hello(@RequestParam(defaultValue = "subway") String search, Model model) {
 		SearchResults searchResults = twitter.searchOperations().search(search);
@@ -38,5 +44,23 @@ public class HelloController {
 		model.addAttribute("tweets", tweets);
 		model.addAttribute("search", search);
 		return "resultPage";
+	}
+
+//	@RequestMapping(value = "/postSearch", method = RequestMethod.POST)
+//	public String postSearch(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+//		String search = request.getParameter("search");
+//		redirectAttributes.addAttribute("search", search);
+//		return "redirect:result";
+//	}
+	
+	@RequestMapping(value = "/postSearch", method = RequestMethod.POST)
+	public String postSearch(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+		String search = request.getParameter("search");
+		if (search.toLowerCase().contains("struts")) {
+			redirectAttributes.addFlashAttribute("error", "Try using spring instead!");
+			return "redirect:/";
+		}
+		redirectAttributes.addAttribute("search", search);
+		return "redirect:result";
 	}
 }
