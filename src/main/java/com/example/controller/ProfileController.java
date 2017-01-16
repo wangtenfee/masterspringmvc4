b2e7ebbe8ee0f1e1ae4d0c3db4aad20cb221609d
wangtenfee <wangtenfee@163.com>
2017-01-16 15:11:02 +0800

    进一步配置了profile,添加了日志配置

--- src/main/java/com/example/controller/ProfileController.java
+++ src/main/java/com/example/controller/ProfileController.java
@@ -2,11 +2,20 @@ package com.example.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.doman.ProfileForm;
 
 @Controller
 public class ProfileController {
 	@RequestMapping("/profile")
 	public String displayProfile() {
 		return "profile/profilePage";
+	}
+
+	@RequestMapping(value = "/profile", method = RequestMethod.POST)
+	public String saveProfile(ProfileForm profileForm) {
+		System.out.println("save ok" + profileForm);
+		return "redirect:/profile";
 	}
 }
