767ed04c5b4f0c5caac78a1134e84216d52a7958
wangtenfee <wangtenfee@163.com>
2017-01-19 19:09:51 +0800

    upload error handling

--- src/main/java/com/example/config/WebConfiguration.java
+++ src/main/java/com/example/config/WebConfiguration.java
@@ -2,9 +2,15 @@ package com.example.config;
 
 import java.time.LocalDate;
 
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -14,6 +20,7 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import com.example.date.USLocalDateFormatter;
 
 @Configuration
+@EnableConfigurationProperties({PictureUploadProperties.class})
 public class WebConfiguration extends WebMvcConfigurerAdapter {
 
 	@Override
@@ -36,5 +43,21 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(localeChangeInterceptor());
+	}
+	
+//	@Bean
+//	public EmbeddedServletContainerCustomizer containerCustomizer() {
+//		EmbeddedServletContainerCustomizer embeddedServletContainerCustomizer = new EmbeddedServletContainerCustomizer() {
+//			@Override
+//			public void customize(ConfigurableEmbeddedServletContainer container) {
+//				container.addErrorPages(new ErrorPage(MultipartException.class, "/uploadError"));
+//			}
+//		};
+//		return embeddedServletContainerCustomizer;
+//	}
+	
+	@Bean //lambda
+	public EmbeddedServletContainerCustomizer containerCustomizer() {
+		return container -> container.addErrorPages(new ErrorPage(MultipartException.class, "/uploadError"));
 	}
 }
--- src/main/java/com/example/profile/PictureUploadController.java
+++ src/main/java/com/example/profile/PictureUploadController.java
@@ -1,49 +1,107 @@
 package com.example.profile;
 
 import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.util.WebUtils;
+
+import com.example.config.PictureUploadProperties;
 
 import java.io.*;
+import java.net.URLConnection;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
+@SessionAttributes("picturePath")
 public class PictureUploadController {
-	public static final Resource PICTURES_DIR = new FileSystemResource("./pictures");
+    private final Resource picturesDir;
+    private final Resource anonymousPicture;
 
-	@RequestMapping("upload")
-	public String uploadPage() {
-		return "profile/uploadPage";
+    @Autowired
+    public PictureUploadController(PictureUploadProperties uploadProperties) {
+        picturesDir = uploadProperties.getUploadPath();
+        anonymousPicture = uploadProperties.getAnonymousPicture();
+    }
+
+    @ModelAttribute("picturePath")
+    public Resource picturePath() {
+        return anonymousPicture;
+    }
+
+    @RequestMapping("upload")
+    public String uploadPage() {
+        return "profile/uploadPage";
+    }
+
+    @RequestMapping(value = "/uploadedPicture")
+    public void getUploadedPicture(HttpServletResponse response,@ModelAttribute("picturePath") Resource picturePath) throws IOException {
+        response.setHeader("Content-Type", URLConnection.guessContentTypeFromName(picturePath.getFilename()));
+        IOUtils.copy(picturePath.getInputStream(), response.getOutputStream());
+    }
+
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    public String onUpload(MultipartFile file, RedirectAttributes redirectAttrs, Model model) throws IOException {
+
+        if (file.isEmpty() || !isImage(file)) {
+            redirectAttrs.addFlashAttribute("error", "Incorrect file. Please upload a picture.");
+            return "redirect:/upload";
+        }
+
+        Resource picturePath = copyFileToPictures(file);
+        model.addAttribute("picturePath", picturePath);
+
+        return "profile/uploadPage";
+    }
+    
+    //handling upload error
+//	@RequestMapping(value = "/upload", method = RequestMethod.POST)
+//	public String onUpload(MultipartFile file, RedirectAttributes redirectAttrs, Model model) throws IOException {
+//		throw new IOException("Some message");
+//	}
+
+    private Resource copyFileToPictures(MultipartFile file) throws IOException {
+        String fileExtension = getFileExtension(file.getOriginalFilename());
+        File tempFile = File.createTempFile("pic", fileExtension, picturesDir.getFile());
+        try (InputStream in = file.getInputStream();
+             OutputStream out = new FileOutputStream(tempFile)) {
+
+            IOUtils.copy(in, out);
+        }
+        return new FileSystemResource(tempFile);
+    }
+
+    private boolean isImage(MultipartFile file) {
+        return file.getContentType().startsWith("image");
+    }
+
+    private static String getFileExtension(String name) {
+        return name.substring(name.lastIndexOf("."));
+    }
+    
+	@ExceptionHandler(IOException.class)
+	public ModelAndView handleIOException(IOException exception) {
+		ModelAndView modelAndView = new ModelAndView("profile/uploadPage");
+		modelAndView.addObject("error", exception.getMessage());
+		return modelAndView;
 	}
-
-	@RequestMapping(value = "/upload", method = RequestMethod.POST)
-	public String onUpload(MultipartFile file, RedirectAttributes redirectAttrs) throws IOException {
-		if (file.isEmpty() || !isImage(file)) {
-			redirectAttrs.addFlashAttribute("error", "Incorrect file.Please upload a picture.");
-			return "redirect:/upload";
-		}
-		copyFileToPictures(file);
-		return "profile/uploadPage";
-	}
-
-	private Resource copyFileToPictures(MultipartFile file) throws IOException {
-		String fileExtension = getFileExtension(file.getOriginalFilename());
-		File tempFile = File.createTempFile("pic", fileExtension, PICTURES_DIR.getFile());
-		try (InputStream in = file.getInputStream(); OutputStream out = new FileOutputStream(tempFile)) {
-			IOUtils.copy(in, out);
-		}
-		return new FileSystemResource(tempFile);
-	}
-
-	private boolean isImage(MultipartFile file) {
-		return file.getContentType().startsWith("image");
-	}
-
-	private static String getFileExtension(String name) {
-		return name.substring(name.lastIndexOf("."));
+	
+	@RequestMapping("uploadError")
+	public ModelAndView onUploadError(HttpServletRequest request) {
+		ModelAndView modelAndView = new ModelAndView("uploadPage");
+		modelAndView.addObject("error", request.getAttribute(WebUtils.ERROR_MESSAGE_ATTRIBUTE));
+		return modelAndView;
 	}
 }
