47438ca7b7a8b73a354e80d2c17dcc9f18df2091
wangtenfee <wangtenfee@163.com>
2017-01-22 14:45:47 +0800

    create user package,add the related code files;

--- src/main/java/com/example/search/SearchService.java
+++ src/main/java/com/example/search/SearchService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.twitter.api.Twitter;
 import org.springframework.stereotype.Service;
 import org.springframework.social.twitter.api.SearchParameters;
-import org.springframework.social.twitter.api.Tweet;
 
 @Service
 public class SearchService {
---
+++ src/main/java/com/example/user/api/UserApiController.java
@@ -0,0 +1,41 @@
+package com.example.user.api;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.user.User;
+import com.example.user.UserRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class UserApiController {
+	private UserRepository userRepository;
+
+	@Autowired
+	public UserApiController(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	@RequestMapping(value = "/users", method = RequestMethod.GET)
+	public List<User> findAll() {
+		return userRepository.findAll();
+	}
+
+	@RequestMapping(value = "/users", method = RequestMethod.POST)
+	public User createUser(@RequestBody User user) {
+		return userRepository.save(user);
+	}
+
+	@RequestMapping(value = "/user/{email}", method = RequestMethod.PUT)
+	public User updateUser(@PathVariable String email, @RequestBody User user) {
+		return userRepository.save(email, user);
+	}
+
+	@RequestMapping(value = "/user/{email}", method = RequestMethod.DELETE)
+	public void deleteUser(@PathVariable String email) {
+		userRepository.delete(email);
+	}
+}
